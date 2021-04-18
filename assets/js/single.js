c669a0ef259dc3c39780d459598a21fabd6e70f0
wadep <wadepfacer@gmail.com>
2021-04-17 21:52:55 -0600

    finished display issue function and single repo page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,5 @@
+var issueContainerEl = document.querySelector('#issues-container');
+
 var getRepoIssues = function(repo){
     console.log(repo);
     var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
@@ -16,13 +18,34 @@ var getRepoIssues = function(repo){
 };
 
 var displayIssues = function(issues){
+    if (issues.length === 0){
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+
     for (var i = 0; i < issues.length; i++){
         // create a link to element to take users to the issue on github
         var issueEl = document.createElement('a');
         issueEl.classList = 'list-item flex-row justify-space-between align-center';
         issueEl.setAttribute('href', issues[i].html_url);
         issueEl.setAttribute('target', '_blank');
+
+        var titleEl = document.createElement('span');
+        titleEl.textContent = issues[i].title;
+
+        issueEl.appendChild(titleEl);
+
+        var typeEl = document.createElement('span');
+
+        // check if issue is actual issue or pull request
+        if (issues[i].pull_request){
+            typeEl.textContent = "(Pull Request)";            
+        }else{
+            typeEl.textContent = "(Issue)";
+        }
+        issueEl.appendChild(typeEl);
+        issueContainerEl.appendChild(issueEl);
     }
 };
 
-getRepoIssues('Facebook/react');
+getRepoIssues('wadep23/git-it-done');
